743169303a2fd35e693a7ddf0e13032146a4715f
Zainab <Zainuuuu07@gmail.com>
2024-09-01 02:06:30 +0530

    responsive design

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import wallpaper from '../images/wallpaper.jpg';
+import React from "react";
+import wallpaper from "../images/wallpaper.jpg";
 
 function Home() {
-  //const navigate = useNavigate();
   return (
     <main>
-    <div className= 'container-word'>
-    <div className="container_content">
-    <div className="container_content_inner">
-    <div className="title">
-      <p>Hello!</p>
-      <h1 className='name'>I'm Zainab Ghadiyali</h1>
-    </div>
-    <div className="par">
-    <p className='developer'>
-    Aspiring Front-End Developer |<br/>
-    Passionate About Creating Engaging <br />
-    User Experiences 
-    </p>
-    </div>
-    <div className="btns">
-      <a href='https://www.linkedin.com/in/zainab-ghadiyali-671665282'>
-    <button className='btns_more' >Linkedin</button>
-    </a>
-    </div>
-    </div>
-    </div>
-     <div className="container_outer_img">
-     <div className="img-inner">
-     <img src={wallpaper} width={500} height={500} alt="" className="container_img"/>
-           </div>
-         </div>
+      <div className="container-word">
+        <div className="container_content">
+          <div className="container_content_inner">
+            <div className="title">
+              <p>Hello!</p>
+              <h1 className="name">I'm Zainab Ghadiyali</h1>
+            </div>
+            <div className="par">
+              <p className="developer">
+                Aspiring Front-End Developer |<br />
+                Passionate About Creating Engaging <br />
+                User Experiences
+              </p>
+            </div>
+            <div className="btns">
+              <a href="https://www.linkedin.com/in/zainab-ghadiyali-671665282">
+                <button className="btns_more">Linkedin</button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="container_outer_img">
+          <div className="img-inner">
+            <img
+              src={wallpaper}
+              width={500}
+              height={500}
+              alt=""
+              className="container_img"
+            />
+          </div>
+        </div>
       </div>
-    <div className="overlay"></div>
-</main>
-     
-    
+      <div className="overlay"></div>
+    </main>
+
     /*<main className="container">
       
     <div className="main-content">
@@ -68,8 +72,7 @@ function Home() {
      
   </main>
 */
-  
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 function Navbar() {
   const location = useLocation();
-  const[activeLink,setActiveLink]=useState(location.pathname);
+  const activeLink = location.pathname;
 
-  useEffect(() => {
-    setActiveLink(location.pathname);
-  }, [location.pathname]);
-
-  
- const handleLinkClick = (link) => {
-  setActiveLink(link);
- };
-  
   return (
-    
-
     <div className="navbar">
       <div className="navbar-left">
         <h5>Zainab Ghadiyali</h5>
       </div>
-      
       <div className="navbar-right">
-        <Link to="my-portfolio/" className={activeLink === '/'? 'active':''} onClick={() => handleLinkClick('/')} > Home</Link>
-        <Link to="my-portfolio/about" className={activeLink === '/about'? 'active':''} onClick={() => handleLinkClick('about')}>About </Link>
-        <Link to="my-portfolio/project"className={activeLink === '/project'? 'active':''} onClick={() => handleLinkClick('project')}>Projects</Link>
+        <Link
+          to="my-portfolio/"
+          className={activeLink === "/my-portfolio/" ? "active" : ""}
+        >
+          {" "}
+          Home
+        </Link>
+        <Link
+          to="my-portfolio/about"
+          className={activeLink === "/my-portfolio/about" ? "active" : ""}
+        >
+          About{" "}
+        </Link>
+        <Link
+          to="my-portfolio/project"
+          className={activeLink === "/my-portfolio/project" ? "active" : ""}
+        >
+          Projects
+        </Link>
       </div>
-      </div>
-      
-    
-    
-  )
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
